876470e8995d00e41bfee282a6d53210aef23143
Boris Alennikov <AlennikovBoris@gmail.com>
2021-10-04 15:01:52 +0300

    Lab--3

---
+++ cmake-build-debug/CMakeFiles/Lab__3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab__3.dir/main.cpp.obj"
+  "Lab__3.exe"
+  "Lab__3.exe.manifest"
+  "Lab__3.lib"
+  "Lab__3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab__3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include<iomanip>
+#include<conio.h>
+#include<vector>
+#include<cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void RandomMatrix(int size,vector<vector<int>>& matrix){
+    srand(time(NULL));
+    int max;
+    cout<<"Enter a max matrix element: ";
+    cin>>max;
+    for(int i = 0; i < size; i++){
+        cout<<"|";
+        for(int j = 0; j < size; j++){
+            matrix[i][j]=rand() % max;
+            if((i+j)%2!=0)
+            {
+                matrix[i][j]=1;
+            }
+            cout<<setw(6)<<matrix[i][j]<<"  ";
+        }
+        cout<<"|";
+        cout<<endl;
+    }
+}
+void HandsMatrix(int size,vector<vector<int>>& matrix){
+    int q=1;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            if((i+j)%2!=0)
+            {
+                matrix[i][j]=1;
+                q++;
+            }
+            else{
+                cout<< "\nEnter matrix elements";
+                cin>>matrix[i][j];
+            }
+        }
+    }
+    for(int k = 0;k < size;k++){
+            cout<<"|";
+            for(int h = 0;h < size;h++)
+            {
+                cout<<setw(6)<<matrix[k][h]<<"  ";
+            }
+           cout<<"|"<<endl;
+        }
+}
+void SumWithoutZero(int size,vector<vector<int>>& matrix){
+        int pos = 0;
+        int sum = 0;
+        for(int i =0;i<size;i++)
+        {
+            for(int j =0;j<size;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    pos++;
+                }
+            }
+            if(pos==0)
+            {
+                for(int t=0;t<size;t++)
+                {
+                    sum=sum + matrix[i][t];
+                }
+                cout<<"Sum of "<<(i+1)<<" row: "<<sum<<endl;
+                sum = 0;
+            }
+            pos= 0;
+        }
+}
+void DiagMultiply(int size,vector<vector<int>>& matrix){
+    int rez = 1;
+    int mult = 0;
+    int y;
+
+    for(int b = 1;b < (size-1);b++){
+        for(int u = b,y = 0;u < size;u++,y++)
+        {
+            rez*=matrix[y][u];
+        }
+        if(rez > mult){
+            swap(rez, mult);
+        }
+        rez=1;
+    }
+    int mult1 = 1;
+    int c;
+    int mult2 = 0;
+    for(int b = 1;b<(size-1);b++){
+        for(int v =0 ,c= b;c<size;c++,v++){
+            mult1*=matrix[c][v];
+        }
+        if(mult1>mult2){
+            swap(mult1,mult2);
+        }
+        mult1=1;
+    }
+    if(mult2>rez){
+        cout<<"The max product of the elements of diagonals parallel to the main: "<<mult2;
+    }
+
+    else{
+        cout<<"The max product of the elements of diagonals parallel to the main: "<<rez;
+    }
+
+}
+
+int main() {
+    int choise;
+    int size;
+    cout << "Enter size of your vector:";
+    cin >> size;
+    while(size < 0 || size > 11){
+        cout << "Size has to be positive and less than eleven :";
+        cin >> size;
+    }
+
+
+    vector< vector <int> >matrix(size,vector <int> (size));
+    cout << "You want to fill  by hands?(Yes(Enter 1) or No(Enter 0)) :";
+    cin >> choise;
+    while (choise < 0 || choise > 1){
+        cout << "input 0 or 1\n";
+        cin >> choise;
+    }
+    if (choise == 1){
+        HandsMatrix(size, matrix);
+    }
+    if (choise == 0){
+        RandomMatrix(size, matrix);
+    }
+    SumWithoutZero(size,matrix);
+    DiagMultiply(size, matrix);
+
+    return 0;
+}
